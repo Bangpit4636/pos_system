29762f502aa422186bcba24a0def91dd9b698b4c
Bangpit4636 <ayamgepukbangpit@gmail.com>
2025-11-11 22:25:14 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,8 @@
 const salesData = JSON.parse(localStorage.getItem("sales")) || [];
-const total = salesData.reduce((sum, s) => sum + s.amount, 0);
-document.getElementById("totalSales").innerText = "RM " + total.toFixed(2);
+const totalSales = salesData.reduce((sum, s) => sum + s.amount, 0);
+
+const expenseData = JSON.parse(localStorage.getItem("expenses")) || [];
+const totalExpense = expenseData.reduce((sum, e) => sum + e.amount, 0);
+
+document.getElementById("totalSales").innerText = "RM " + totalSales.toFixed(2);
+document.getElementById("totalExpense").innerText = "RM " + totalExpense.toFixed(2);
